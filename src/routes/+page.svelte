6405578f794382f60d3fe22eec608c6d71b00337
lukasvasadi <lukas.vasadi@gmail.com>
2026-09-20 13:18:00 -0400

    Decrease heading line height to 1.2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,7 @@
 	<ul class="posts">
 		{#each data.posts as post (post.slug)}
 			<li class="post">
-				<a href={post.slug} class="title">{post.title}</a>
+				<h2 class="title"><a href={post.slug}>{post.title}</a></h2>
 				<p class="date">{formatDate(post.date)}</p>
 				<p class="description">{post.description}</p>
 			</li>
@@ -44,6 +44,7 @@
 	.title {
 		font-size: clamp(2rem, 9vw, 3.5rem);
 		font-family: 'Playfair Display', sans-serif;
+		font-weight: 400;
 		/*text-transform: capitalize;*/
 	}
 
